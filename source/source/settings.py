d4660a30c7dd35ad80993b73c55e9e8746c66283
Nhu Vu <geniuslab00@gmail.com>
2023-10-02 22:07:51 -0700

    some initial skeleton code

--- source/source/settings.py
+++ source/source/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = False  # Set to False to specify allowed origins
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Add your frontend URL here
+    "https://internpedia.com",
+]
