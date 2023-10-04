4049dd45001a44a2edd808ca86030b968d4f262f
Nhu Vu <geniuslab00@gmail.com>
2023-10-04 14:49:47 -0700

    installed some dependencies, nhu set up her admin user,some initial code in models.py and settings.py

--- development/source/settings.py
+++ development/source/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'internpedia'
+    'corsheaders',
+    'rest_framework',
+    'internpedia',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'source.urls'
